74d402a345f8bbc73c2cbb2840c6c06400f274c0
Sumaya <sumayajaabo07@gmail.com>
2024-08-29 09:41:53 +0300

    step 3 completed

--- src/app/components/job-details/job-details.component.ts
+++ src/app/components/job-details/job-details.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { JobsService } from '../../services/jobs.service';
 import { Job } from '../../interfaces/job.interface';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-job-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './job-details.component.html',
   styleUrl: './job-details.component.css'
 })
